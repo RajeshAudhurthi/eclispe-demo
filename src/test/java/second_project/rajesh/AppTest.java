ddbf9f0608bda063021dab722efbae998e941708
Administrator <admin@example.com>
2021-10-29 16:09:47 +0530

    first push

---
+++ src/test/java/second_project/rajesh/AppTest.java
@@ -0,0 +1,20 @@
+package second_project.rajesh;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
